ec3e02c3a73af1d1846950c52795f2f2f70902e0
Mohammed Soharab S <soharab@quinta.co.in>
2022-07-04 00:33:00 +0530

    new changed

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.post('/signup',urlencodedParser,(req,res)=>{
 app.get('/login',(req,res)=>{
 	res.render('login')
 })
+let email_id
 app.post('/login',urlencodedParser,(req,res)=>{
 	validateLogin(req.body.email, req.body.passcode).then(function (obj) {
         console.log(obj);
@@ -56,22 +57,31 @@ app.post('/login',urlencodedParser,(req,res)=>{
         else 
             return res.render('login',{error:"Email or password is wrong"})
         
-    })
+    }).then(function setEmailID(email = req.body.email){
+        email_id = email
+    }).catch((err)=> res.send("<h3>Something went wrong!</h3>"))
 })
+
 app.get('/portfolio',(req,res)=>{
-	res.render('portfolio')
+	getPortfolio(email_id).then((data)=>{
+        console.log(data['data'][0])
+        res.render('portfolio',stock = data['data'][0])
+    }).catch((err)=> res.send("<h3>Something went wrong!</h3> "))
+    
 })
 app.get('/trade',(req,res)=>{
 	res.render('trade')
 })
-app.post('/trade',(req,res)=>{
-	res.redirect('/portfolio')
+app.post('/trade',urlencodedParser,(req,res)=>{
+	let stockObj = Object.assign({},req.body) 
+    console.log(Object.assign({},req.body))
+    updateHoldings(email_id,stockObj.stock,stockObj.amount).then((obj)=>console.log(obj)).catch((err)=>console.log(err))
+    res.redirect('portfolio')
 })
 app.listen(port, () => console.log("listening"))
 
 
 function validateLogin(email, passcode) {
-    //prescription.unique_id = id
     return new Promise((resolve, reject) => {
         let sql = ' SELECT email FROM StockUser WHERE email="'+email+'" AND passcode ="'+passcode+'"'
 	    console.log(sql);
@@ -85,5 +95,55 @@ function validateLogin(email, passcode) {
                 flag: result[0]
             })
         })
+    })
+}
+
+function getPortfolio(email){
+    return new Promise((resolve, reject) => {
+        let sql = ' SELECT * FROM Holdings WHERE email = "'+email+'"'
+	    console.log(sql);
+        con.query(sql, (err, result) => {
+            if (err ) reject({
+                sqlerror:err,
+                query:sql
+            })
+            //console.log(result);
+            resolve({
+                data: result
+            })
+        })
+    })
+
+}
+
+function updateHoldings(email,token,value){
+    return new Promise((resolve, reject) => {
+        let sql1 = 'INSERT INTO Holdings(email) VALUES("'+email+'")'
+	    console.log(sql1)
+        con.query(sql1, (err, result) => {
+            if (err ) reject({
+                sqlerror:err,
+                query:sql1
+            })
+            //console.log(result);
+            resolve({
+                data: result
+            })
+        })
+
+        let sql2 = 'UPDATE Holdings SET '+token+' = "'+value+'" WHERE email="'+email+'"'
+	    console.log(sql2)
+        con.query(sql2, (err, result) => {
+            if (err ) reject({
+                sqlerror:err,
+                query:sql2
+            })
+            //console.log(result);
+            resolve({
+                data: result
+            })
+        })
+        
+
     })
 }
